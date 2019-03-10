551bba6a1f4ee64305b62c04948344ed71b11696
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2019-03-10 11:22:25 +0300

    :memo: log ioc resolutions

--- src/ioc.ts
+++ src/ioc.ts
@@ -9,6 +9,24 @@ import logger from './logger'
 
 let iocContainer = new Container()
 
+iocContainer.applyMiddleware(planAndResolve => args => {
+  let serviceId =
+    typeof args.serviceIdentifier === 'function'
+      ? args.serviceIdentifier.name
+      : args.serviceIdentifier.toString()
+
+  logger.trace(`ioc: start resolving "${serviceId}"`)
+
+  let start = process.hrtime()
+
+  let result = planAndResolve(args)
+
+  let [sec, nanoSec] = process.hrtime(start)
+  logger.debug(`ioc: resolved "${serviceId}" in ${sec}s ${nanoSec / 1000000}ms`)
+
+  return result
+})
+
 iocContainer.bind(Types.Logger).toConstantValue(logger)
 
 iocContainer.bind(HeroService).toSelf()
